147a89392768092b07d610c1222f7f6f6bf472ef
Crispin <cc17g12@ecs.soton.ac.uk>
2014-12-15 15:07:03 +0000

    Added randomly fill pot method

---
+++ Tournament ABM/Main.java
@@ -0,0 +1,12 @@
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		Population pop = new Population(120);
+		
+		pop.randomlyFillPots(12, 10);
+		
+		System.out.println("done");
+	}
+}
